8a31e9f6d446d3fd74fd31ebdb6fe1ad82e3d191
Patryk Omiotek <patryk.omiotek@gmail.com>
2020-12-22 14:49:43 +0100

    first fetch

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ function App() {
       setSurname(value);
     }
   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, surname);
+    fetch('https://a24b8d9cbdbcaaca93f2fc3eff6e913e.m.pipedream.net', { // TODO: fixme
+      method: 'POST',
+      body: JSON.stringify({ name, surname }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+  };
   return (
     <div className="App">
       <Menu>
@@ -27,14 +38,17 @@ function App() {
       </Menu>
       <div>
         <h1>Hi {name} {surname}!</h1>
-        <input
-          type="text"
-          name="name"
-          onChange={handleChange} />
-        <input
-          type="text"
-          name="surname"
-          onChange={handleChange} />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange} />
+          <input
+            type="text"
+            name="surname"
+            onChange={handleChange} />
+          <input type="submit" value="Send" />
+        </form>
       </div>
       <Footer />
     </div>
