ca5bb687a384dee0dd68e33a3ab6e7be75e61a8d
Patryk Omiotek <patryk.omiotek@gmail.com>
2020-12-22 14:27:54 +0100

    second field

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ import Footer from './components/Footer';
 
 function App() {
   const [name, setName] = useState('Patryk');
+  const [surname, setSurname] = useState('O');
+
   const handleChange = (event) => {
-    setName(event.target.value);
+    const name = event.target.name;
+    const value = event.target.value;
+    if (name === 'name') { // TODO: refactor
+      setName(value);
+    } else if (name === 'surname') {
+      setSurname(value);
+    }
   };
   return (
     <div className="App">
@@ -18,8 +26,15 @@ function App() {
         <Link url="https://interia.pl">Interia</Link>
       </Menu>
       <div>
-        <h1>Hi {name}!</h1>
-        <input type="text" onChange={handleChange} />
+        <h1>Hi {name} {surname}!</h1>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange} />
+        <input
+          type="text"
+          name="surname"
+          onChange={handleChange} />
       </div>
       <Footer />
     </div>
