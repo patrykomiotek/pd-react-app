6a8384099e78204d72e3a26f82cc6bd319b08662
Patryk Omiotek <patryk.omiotek@gmail.com>
2020-12-22 16:52:20 +0100

    day1

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ function App() {
   const [users, setUsers] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const nameInputRef = useRef(null);
+  const nameInputErrorLabelRef = useRef(null);
+  const buttonRef = useRef(null);
 
   useEffect(() => {
     setTimeout(() => {
@@ -45,17 +47,19 @@ function App() {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(name, surname);
     nameInputRef.current.style.border = '#f00 1px solid';
     nameInputRef.current.value = '>!>!>!>!';
-
-    fetch('https://a24b8d9cbdbcaaca93f2fc3eff6e913e.m.pipedream.net', { // TODO: fixme
-      method: 'POST',
-      body: JSON.stringify({ name, surname }),
-      headers: {
-        'Content-type': 'application/json'
-      }
-    });
+    if (name === '') {
+      nameInputErrorLabelRef.current.style.display = 'block';
+    } else {
+      fetch('https://a24b8d9cbdbcaaca93f2fc3eff6e913e.m.pipedream.net', { // TODO: fixme
+        method: 'POST',
+        body: JSON.stringify({ name, surname }),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      });
+    }
   };
   const showIndicator = () => {
     // {isLoading ? <h4>Loading...</h4> : null}
@@ -76,18 +80,27 @@ function App() {
       <div>
         <h1>Hi {name} {surname}!</h1>
         <form onSubmit={handleSubmit}>
+
+          {/* <Field type="text" name="name" required /> */}
+
           <input
             type="text"
             name="name"
             ref={nameInputRef}
             defaultValue={name}
             onChange={handleChange} />
+          <div ref={nameInputErrorLabelRef} style={{ display: 'none' }}>Field is required</div>
           <input
             type="text"
             name="surname"
             value={surname}
             onChange={handleChange} />
-          <input type="submit" value="Send" />
+          <input
+            type="submit"
+            value="Send"
+            ref={buttonRef}
+            onMouseOver={() => buttonRef.current.style.backgroundColor = '#00f'}
+            onMouseOut={() => buttonRef.current.style.backgroundColor = '#0f0'} />
         </form>
       </div>
       <hr />
