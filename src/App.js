fcb87c1d939a51880c1fcfa998c234e3113098c7
Patryk Omiotek <patryk.omiotek@gmail.com>
2020-12-22 12:55:34 +0100

    link component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import './App.css';
+
+function Link(props) {
+  const { url, children } = props;
+  return <a href={url}>{children}</a>;
+}
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <Link url="https://onet.pl">Onet</Link>
+      <Link url="https://wp.pl">WP</Link>
+      <Link url="https://interia.pl">Interia</Link>
+    </div>
+  );
+}
+
+function Footer() {
+  return <div className="Footer">&copy; 2020 by Ja</div>;
+}
+
 function App() {
   return (
     <div className="App">
-
+      <Menu />
+      <Footer />
     </div>
   );
 }
