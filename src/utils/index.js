eb70bc53b1fcd08a0fca22b4e77b5760230d0a2c
Patryk Omiotek <patryk.omiotek@gmail.com>
2020-12-23 13:55:06 +0100

    refactor

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 function objectToArray(obj) {
   const arr = [];
   Object.entries(obj).forEach(elem => {
-    const user = { id: elem[0], ...elem[1] };
-    arr.push(user);
+    const item = { id: elem[0], ...elem[1] };
+    arr.push(item);
   });
   return arr;
 }
