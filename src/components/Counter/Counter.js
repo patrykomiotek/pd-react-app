98cf068cce3e037e1119c69e3d548feb1dcd5be2
Patryk Omiotek <patryk.omiotek@gmail.com>
2020-12-28 11:32:35 +0100

    counter with callback

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const decrement = () => setCount(count - 1);
-  const increment = () => setCount(count + 1);
+  const decrement = () => setCount((c) => c - 1);
+  const increment = () => setCount((c) => c + 1);
   return (
     <div>
       <p>Current value: {count}</p>
