377e566fa758ead45c2ca2467f485e55a490150b
Patryk Omiotek <patryk.omiotek@gmail.com>
2020-12-29 09:35:17 +0100

    css in js

--- src/components/Menu/Link.js
+++ src/components/Menu/Link.js
@@ -1,14 +1,26 @@
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+
+// import {blue} from './colors';
 
 const styles = {
   marginLeft: 6,
   marginRight: 6,
   textDecoration: 'none',
-  color: '#2c3e50;'
+  // color: blue,
+  // ':hover': {
+
+  // }
+  children: {
+    margin: 10,
+  },
 };
 
-function Link({ to, children  }) {
-  return <NavLink to={to} exact style={styles} activeClassName="Link--active">{children}</NavLink>;
+function Link({to, children}) {
+  return (
+    <NavLink to={to} exact style={styles} activeClassName="Link--active">
+      <span style={styles.children}>{children}</span>
+    </NavLink>
+  );
   // return <NavLink to={to} exact className="Link" activeClassName="active">{children}</NavLink>;
 }
 
---
+++ src/components/Menu/colors.js
@@ -0,0 +1,6 @@
+export const red = '#f00';
+export const blue = '#2980b9';
+
+// primaryColor
+// secondaryColor
+// headerColor
